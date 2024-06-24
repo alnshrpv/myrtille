33f65c182b218402541d04aeb2dfd7004d590efe
alnshrpv <124539305+alnshrpv@users.noreply.github.com>
2024-06-25 04:16:02 +0800

    m

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,37 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
-})
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    }
+  },
+
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].hash = true;
+      return args;
+    });
+  },
+
+  configureWebpack: {
+    output: {
+      filename: 'js/[name].[contenthash:8].js',
+      chunkFilename: 'js/[name].[contenthash:8].js'
+    }
+  },
+
+  devServer: {
+    historyApiFallback: true,
+  },
+
+  outputDir: 'dist',
+
+  assetsDir: 'assets',
+
+  filenameHashing: true,
+
+  productionSourceMap: false,
+});
